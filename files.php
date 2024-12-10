d5c741cb93e9941172af58f44ef9547b5f6664e9
AnouarEssabiri <anouaressabiri@gmail.com>
2024-12-11 00:02:12 +0100

    editing style fies and home pages

--- files.php
+++ files.php
@@ -44,10 +44,10 @@ $files = $conn->query("SELECT * FROM files where folder_id = $folder_parent and 
 		background: #80808024;
 	}
 
-	table th,
+	/* table th,
 	td {
-		/*border-left:1px solid gray;*/
-	}
+		border-left:1px solid gray;
+	} */
 
 	a.custom-menu-list span.icon {
 		width: 1em;
